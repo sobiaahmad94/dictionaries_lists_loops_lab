0071d685f0016fbc3fa97387fcf63bd091c8d08e
sobiaahmad94 <sobiaahmad94@icloud.com>
2023-04-04 23:44:36 +0100

    completed some more questions

--- start_point/exercise_b_users.py
+++ start_point/exercise_b_users.py
@@ -55,10 +55,27 @@ users = {
 }
 
 # 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
+# users is a dict, Jonathan is a dict inside the users dict
+# key/value pairs
+# twitter is the key, johnnyt is the value, I need to grab the key to get the value
+twitter_username = (users["Jonathan"]["twitter"])
+print(twitter_username) 
+
 # 2. Get Erik's hometown
+eriks_home_town = (users["Erik"]["home_town"])
+print(eriks_home_town) 
+
 # 3. Get the list of Erik's lottery numbers
+eriks_lottery = (users["Erik"]["lottery_numbers"])
+print(eriks_lottery)
+
 # 4. Get the species of Avril's pet Monty
+# avrils_pet_species = (users["Avril"]["pets"[2]]) # I'm stuck on this 
+# print(avrils_pet_species) 
+
 # 5. Get the smallest of Erik's lottery numbers
+smallest_eriks_lottery = (users["Erik"]str(["lottery_numbers"].split())) # I'm stuck on this 
+print(smallest_eriks_lottery)
 # 6. Return an list of Avril's lottery numbers that are even
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
 # 8. Change Erik's hometown to Edinburgh
